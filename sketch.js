bd531ad0c780fd922f269962bd65a8113a0cd579
Pratham Agarwal <71118804+pratham2007@users.noreply.github.com>
2020-10-24 18:42:24 +0530

    added jumping condition

--- sketch.js
+++ sketch.js
@@ -77,7 +77,7 @@ function draw() {
     score = score + Math.round(getFrameRate() / 60);
     ground.velocityX = -(6 + 3 * score / 100);
 
-    if (keyDown("space") || touches.length>0) {
+    if (trex.y > height-55 && keyDown("space") || touches.length>0) {
       trex.velocityY = -12;
       touches = [];
     }
